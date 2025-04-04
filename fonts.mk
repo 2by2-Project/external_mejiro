d16e101efd7062c5407f294025826bc1136edbc0
kailua <kailua@project2by2.jp>
2025-04-04 19:52:37 +0900

    mejiro: Add fonts.mk

---
+++ fonts.mk
@@ -0,0 +1,12 @@
+#
+#  Copyright (C) 2025 The 2by2 Project
+#  SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    Mejiro-Thin.ttf \
+    Mejiro-Light.ttf \
+    Mejiro-Regular.ttf \
+    Mejiro-Semibold.ttf \
+    Mejiro-Bold.ttf \
+    Mejiro-Extrabold.ttf
